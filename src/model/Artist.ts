439527f833b3410e484601facdb635210c8fe815
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-03-17 00:40:39 +0600

    feat(backend): add unique validation for Artist model

--- src/model/Artist.ts
+++ src/model/Artist.ts
@@ -4,6 +4,19 @@ const ArtistSchema = new Schema({
   name: {
     type: String,
     required: true,
+    unique: true,
+    min: 1,
+    validate: {
+      validator: async (name: string) => {
+        const artist = await Artist.findOne({ name });
+        if (artist) {
+          return false;
+        }
+
+        return true;
+      },
+      message: 'Artist already exists',
+    },
   },
   image: {
     type: String,
