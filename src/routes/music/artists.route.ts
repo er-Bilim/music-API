b56f738f1456958e000e1a26b46b0a1b0789c82b
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-03-17 00:41:34 +0600

    feat(backend): implement GET and POST routes for artists

--- src/routes/music/artists.route.ts
+++ src/routes/music/artists.route.ts
@@ -1,11 +1,47 @@
 import { Router, type Request, type Response } from 'express';
+import { imagesUpload } from '../../middlewares/multer.js';
+import type { IArtist } from '../../types/music.types.js';
+import Artist from '../../model/Artist.js';
+import { Error } from 'mongoose';
 
 const artistsRouter = Router();
 
-artistsRouter.get('/', async (req: Request, res: Response) => {
-  return res.json({
-    artist: 'Artist',
-  });
+artistsRouter.get('/', async (req: Request, res: Response, next) => {
+  try {
+    const artists = await Artist.find().select('-__v');
+    return res.json(artists);
+  } catch (error) {
+    return next(error);
+  }
 });
+
+artistsRouter.post(
+  '/',
+  imagesUpload.single('image'),
+  async (req: Request, res: Response, next) => {
+    const body: IArtist = req.body;
+
+    const correctArtistData: IArtist = {
+      name: body.name,
+      information: body.information,
+      image: req.file ? `images/${req.file.filename}` : null,
+    };
+
+    try {
+      const artistData = new Artist(correctArtistData);
+      await artistData.save();
+
+      return res.json(artistData);
+    } catch (error) {
+      if (error instanceof Error.ValidationError) {
+        res.status(400).json({
+          error,
+        });
+      }
+
+      next(error);
+    }
+  },
+);
 
 export default artistsRouter;
