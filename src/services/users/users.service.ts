26569ca24c5d1910df1283c25a5d26c480f44458
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-03-19 23:58:56 +0600

    feat(backend #83): define User schema and model types

--- src/services/users/users.service.ts
+++ src/services/users/users.service.ts
@@ -1,11 +1,30 @@
-import User from '../../model/User.js';
-import type { IUserReg } from '../../types/user.types.js';
+import User from '../../model/user/User.js';
+import type { IUser, IUserReg } from '../../types/user.types.js';
 
 class UsersService {
   static registration = async (data: IUserReg): Promise<IUserReg> => {
     const user = new User(data);
     user.generateAuthToken();
     return await user.save();
+  };
+
+  static authentication = async (
+    username: string,
+    password: string,
+  ): Promise<{ user: IUser | null; isMatch: boolean }> => {
+    const user = await User.findOne({ username });
+    const data = { user, isMatch: false };
+
+    if (user) {
+      const isMatch: boolean = await user.checkPassword(password);
+
+      data.user = user;
+      if (isMatch) {
+        data.isMatch = true;
+      }
+    }
+
+    return data;
   };
 }
 
