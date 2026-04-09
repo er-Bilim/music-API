ad0e79a15b53d7ef3fb51a688dbe4ef53f183d0a
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-04-09 20:51:44 +0600

    feat(backend): setup new fixtures with roles and statuses. closed #23

--- backend/src/fixtures/fixtures.ts
+++ backend/src/fixtures/fixtures.ts
@@ -23,31 +23,56 @@ const run = async () => {
     console.error(error);
   }
 
-  await User.create(
-    {
-      username: 'polskyBobr',
-      password: 'polskyBobr12345',
-      token: randomUUID(),
-    },
-    {
-      username: 'cheeseNagibator',
-      password: 'cheeseNagibator12345',
-      token: randomUUID(),
-    },
-  );
+  const admin = new User({
+    username: 'admin',
+    password: 'admin',
+    role: 'user',
+    token: '',
+  });
 
-  const [metro_boomin, the_weeknd] = await Artist.create(
+  admin.generateAuthToken();
+  await admin.save();
+
+  const polskyBobr = new User({
+    username: 'polskyBobr',
+    password: 'polskyBobr12345',
+    role: 'user',
+    token: '',
+  });
+
+  polskyBobr.generateAuthToken();
+  await polskyBobr.save();
+
+  const cheeseNagibator = new User({
+    username: 'cheeseNagibator',
+    password: 'cheeseNagibator12345',
+    role: 'user',
+  });
+
+  cheeseNagibator.generateAuthToken();
+  await cheeseNagibator.save();
+
+  const [metro_boomin, the_weeknd, dominic_fike] = await Artist.create(
     {
       name: 'Metro Boomin',
       image: `${fixtureImagesPath}/artists/metro_boomin.jpg`,
       information:
         'Leland Tyler Wayne (born September 16, 1993), professionally known as Metro Boomin (also known as Young Metro or simply Metro), is an Atlanta-based record producer and record executive from St. Louis, Missouri. Having expressed an interest for beat-making in his early teens.',
+      isPublished: true,
     },
     {
       name: 'The Weeknd',
       image: `${fixtureImagesPath}/artists/the_weeknd.png`,
       information:
         'Abel Makkonen Tesfaye (born February 16, 1990 in Scarborough, Ontario), popularly known as The Weeknd, is an R&B/pop/hip-hop singer-songwriter and record producer from Toronto, Canada. He has been referred to as the songbird of his generation and the best musical talent since Michael Jackson throughout his career.',
+      isPublished: true,
+    },
+    {
+      name: 'Dominic Fike',
+      image: `${fixtureImagesPath}/artists/dominic_fike.png`,
+      information:
+        'Dominic David Fike (born December 30, 1995), also known as Dominic Fike (previously L$G or Dom), is an American singer-songwriter, rapper and producer hailing from Naples, Florida.',
+      isPublished: false,
     },
   );
 
@@ -56,30 +81,43 @@ const run = async () => {
     spdm_across_the_spd_verse,
     starboy,
     the_highlights,
+    sunburn,
   ] = await Album.create([
     {
       artist_id: metro_boomin!.id,
       name: 'Heroes & Villains',
       release_year: 2022,
       image: `${fixtureImagesPath}/albums/heroes_and_villains.png`,
+      isPublished: true,
     },
     {
       artist_id: metro_boomin!.id,
       name: 'Spider-Man: Across the Spider-Verse',
       release_year: 2023,
       image: `${fixtureImagesPath}/albums/spider_man_across_the_spider-verse.png`,
+      isPublished: true,
     },
     {
       artist_id: the_weeknd!.id,
       name: 'Starboy',
       release_year: 2023,
       image: `${fixtureImagesPath}/albums/star_boy.png`,
+      isPublished: true,
     },
     {
       artist_id: the_weeknd!.id,
       name: 'The Highlights',
       release_year: 2021,
       image: `${fixtureImagesPath}/albums/the_highlights.png`,
+      isPublished: true,
+    },
+
+    {
+      artist_id: dominic_fike!.id,
+      name: 'Sunburn',
+      release_year: 2023,
+      image: `${fixtureImagesPath}/albums/sunburn.png`,
+      isPublished: false,
     },
   ]);
 
@@ -90,6 +128,7 @@ const run = async () => {
       time: '3:02',
       trackNumber: 1,
       youtubeLink: 'https://youtu.be/_WCD3Z9UmJ4?list=RD_WCD3Z9UmJ4',
+      isPublished: true,
     },
     {
       album_id: heroes_and_villains!.id,
@@ -97,6 +136,7 @@ const run = async () => {
       time: '3:15',
       trackNumber: 2,
       youtubeLink: 'https://youtu.be/TxUdlC0057s?list=RDTxUdlC0057s',
+      isPublished: true,
     },
     {
       album_id: heroes_and_villains!.id,
@@ -104,6 +144,7 @@ const run = async () => {
       time: '3:11',
       trackNumber: 3,
       youtubeLink: 'https://youtu.be/IjnCoWA9u9M?list=RDIjnCoWA9u9M',
+      isPublished: true,
     },
     {
       album_id: heroes_and_villains!.id,
@@ -111,6 +152,7 @@ const run = async () => {
       time: '3:08',
       trackNumber: 4,
       youtubeLink: 'https://youtu.be/2WqI50mc7Jo?list=RD2WqI50mc7Jo',
+      isPublished: true,
     },
     {
       album_id: heroes_and_villains!.id,
@@ -118,6 +160,7 @@ const run = async () => {
       time: '1:30',
       trackNumber: 5,
       youtubeLink: 'https://youtu.be/mSE70gbmBzw?list=RDmSE70gbmBzw',
+      isPublished: true,
     },
 
     {
@@ -126,6 +169,7 @@ const run = async () => {
       time: '4:16',
       trackNumber: 1,
       youtubeLink: 'https://youtu.be/2xomWWncop0?list=RD2xomWWncop0',
+      isPublished: true,
     },
     {
       album_id: spdm_across_the_spd_verse!.id,
@@ -133,6 +177,7 @@ const run = async () => {
       time: '3:51',
       trackNumber: 2,
       youtubeLink: 'https://youtu.be/EbHhQfTvMSA?list=RDEbHhQfTvMSA',
+      isPublished: true,
     },
     {
       album_id: spdm_across_the_spd_verse!.id,
@@ -140,6 +185,7 @@ const run = async () => {
       time: '2:20',
       trackNumber: 3,
       youtubeLink: 'https://youtu.be/1bAxYmFpIiU?list=RD1bAxYmFpIiU',
+      isPublished: true,
     },
     {
       album_id: spdm_across_the_spd_verse!.id,
@@ -147,6 +193,7 @@ const run = async () => {
       time: '2:00',
       trackNumber: 4,
       youtubeLink: 'https://youtu.be/D5d5xinZI3E?list=RDD5d5xinZI3E',
+      isPublished: true,
     },
     {
       album_id: spdm_across_the_spd_verse!.id,
@@ -154,6 +201,7 @@ const run = async () => {
       time: '3:54',
       trackNumber: 5,
       youtubeLink: 'https://youtu.be/LNIkS0KyJwc?list=RDLNIkS0KyJwc',
+      isPublished: true,
     },
 
     {
@@ -162,6 +210,7 @@ const run = async () => {
       time: '4:33',
       trackNumber: 1,
       youtubeLink: 'https://youtu.be/34Na4j8AVgA?list=RD34Na4j8AVgA',
+      isPublished: true,
     },
     {
       album_id: starboy!.id,
@@ -169,6 +218,7 @@ const run = async () => {
       time: '4:17',
       trackNumber: 2,
       youtubeLink: 'https://youtu.be/diW6jXhLE0E?list=RDdiW6jXhLE0E',
+      isPublished: true,
     },
     {
       album_id: starboy!.id,
@@ -176,6 +226,7 @@ const run = async () => {
       time: '3:26',
       trackNumber: 3,
       youtubeLink: 'https://youtu.be/VQ5XQYpx2mg?list=RDVQ5XQYpx2mg',
+      isPublished: true,
     },
     {
       album_id: starboy!.id,
@@ -183,6 +234,7 @@ const run = async () => {
       time: '3:52',
       trackNumber: 4,
       youtubeLink: 'https://youtu.be/YQ-qToZUybM?list=RDYQ-qToZUybM',
+      isPublished: true,
     },
     {
       album_id: starboy!.id,
@@ -190,6 +242,7 @@ const run = async () => {
       time: '3:40',
       trackNumber: 5,
       youtubeLink: 'https://youtu.be/iBnLoAE9kUE?list=RDiBnLoAE9kUE',
+      isPublished: true,
     },
 
     {
@@ -198,6 +251,7 @@ const run = async () => {
       time: '4:33',
       trackNumber: 1,
       youtubeLink: 'https://youtu.be/34Na4j8AVgA?list=RD34Na4j8AVgA',
+      isPublished: true,
     },
     {
       album_id: the_highlights!.id,
@@ -205,6 +259,7 @@ const run = async () => {
       time: '4:09',
       trackNumber: 2,
       youtubeLink: 'https://youtu.be/JPIhUaONiLU?list=RDJPIhUaONiLU',
+      isPublished: true,
     },
     {
       album_id: the_highlights!.id,
@@ -212,6 +267,7 @@ const run = async () => {
       time: '3:53',
       trackNumber: 3,
       youtubeLink: 'https://youtu.be/YQ-qToZUybM?list=RDYQ-qToZUybM',
+      isPublished: true,
     },
     {
       album_id: the_highlights!.id,
@@ -219,6 +275,7 @@ const run = async () => {
       time: '3:23',
       trackNumber: 4,
       youtubeLink: 'https://youtu.be/fHI8X4OXluQ?list=RDfHI8X4OXluQ',
+      isPublished: true,
     },
     {
       album_id: the_highlights!.id,
@@ -226,6 +283,32 @@ const run = async () => {
       time: '4:09',
       trackNumber: 5,
       youtubeLink: 'https://youtu.be/1DpH-icPpl0?list=RD1DpH-icPpl0',
+      isPublished: true,
+    },
+
+    {
+      album_id: sunburn!.id,
+      name: 'Mona Lisa',
+      time: '3:08',
+      trackNumber: 1,
+      youtubeLink: 'https://youtu.be/d5_V3a_oRnA',
+      isPublished: false,
+    },
+    {
+      album_id: sunburn!.id,
+      name: 'Mama’s Boy',
+      time: '2:54',
+      trackNumber: 2,
+      youtubeLink: 'https://youtu.be/KSXP1DRnTKc',
+      isPublished: false,
+    },
+    {
+      album_id: sunburn!.id,
+      name: 'Think Fast',
+      time: '3:46',
+      trackNumber: 3,
+      youtubeLink: 'https://youtu.be/MFkw_-YbX-0?list=RDMFkw_-YbX-0',
+      isPublished: false,
     },
   ]);
 };
